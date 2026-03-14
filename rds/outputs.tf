da7f4c6d6e7706d08976b71d2c1a83758b596c5e
Gideon Omole <gideonomole9@gmail.com>
2026-03-14 18:32:35 +0100

    Add RDS outputs for database username and name

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -3,3 +3,15 @@ output "rds_endpoint" {
   description = "The RDS database endpoint"
   value       = aws_db_instance.database_instance.endpoint
 }
+
+# RDS username 
+output "rds_db_username" {
+  description = "The RDS database username"
+  value       = aws_db_instance.database_instance.username
+}
+
+# RDS database name
+output "rds_db_name" {
+  description = "The RDS database name"
+  value       = aws_db_instance.database_instance.db_name
+}
